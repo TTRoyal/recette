01a5798ec6c6dbf294a73eadc7b9c1233e12f984
TT Royal <78960032+TTRoyal@users.noreply.github.com>
2025-08-12 16:37:54 +0000

    Ajout du composant Badge et mise à jour du composant Profile pour inclure des badges et des options de déconnexion.

--- recette/src/components/page/header/profile.tsx
+++ recette/src/components/page/header/profile.tsx
@@ -1,19 +1,30 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { Popover, PopoverTrigger } from "@/components/ui/popover";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { useUser } from "@clerk/nextjs";
+import { ChefHat, LogOut, Settings } from "lucide-react";
 
 export default function Profile() {
     const { user } = useUser();
     return (
     <Popover>
-        <PopoverTrigger>qsd
-            <Button className="p-0 rounded-3xl w-5 h-5 hover:bg-primary bg-white hover:cursor-pointer" >
+        <PopoverTrigger className="hover:cursor-pointer">
+            
                 <Avatar>
                     <AvatarImage src={user?.imageUrl} alt={user?.fullName || "User Avatar"} className="" />
                 </Avatar>
-            </Button>
+            
         </PopoverTrigger>
+        <PopoverContent className="mr-4 bg-[radial-gradient(circle_at_50%_0%,theme(colors.white/10%),transparent)] sm:max-w-[205px] p-2">
+            <Avatar className="w-16 h-16 mx-auto mb-2">
+                <AvatarImage src={user?.imageUrl} alt={user?.fullName || "User Avatar"} className="" />
+            </Avatar>
+            <h2 className="font-semibold text-center">{user?.fullName}</h2>
+            <Button variant="ghost" className="w-full justify-start hover:cursor-pointer my-1"> <ChefHat/> Mes Recettes <Badge variant="secondary">20</Badge> </Button>
+            <Button variant="ghost" className="w-full justify-start hover:cursor-pointer my-1"> <Settings/> Paramètres</Button>
+            <Button variant="ghost" className="w-full justify-start hover:cursor-pointer my-1"> <LogOut/> Se Déconnecter</Button>
+        </PopoverContent>
     </Popover>
   );
 }
